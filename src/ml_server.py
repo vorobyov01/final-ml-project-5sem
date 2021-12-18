ac228f76f93f08980352db9148c1745b96381eea
Sergei <admin@vorobyov01.ru>
2021-12-18 14:23:08 +0300

    refactoring

--- src/ml_server.py
+++ src/ml_server.py
@@ -2,7 +2,9 @@ import io
 import pandas as pd
 import numpy as np
 
-from sklearn.ensemble import RandomForestRegressor # <- needs to be replaced
+from ensembles import RandomForestMSE, GradientBoostingMSE
+
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor # <- needs to be replaced
 from sklearn.metrics import mean_squared_error # <- needs to be replaced
 
 from collections import namedtuple
@@ -13,62 +15,101 @@ from flask import render_template, redirect
 
 from flask_wtf.file import FileAllowed
 from wtforms.validators import DataRequired
-from wtforms import StringField, SubmitField, FileField
+from wtforms import StringField, SubmitField, FileField, SelectField
 
 app = Flask(__name__, template_folder='templates')
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 app.config['SECRET_KEY'] = 'sergei'
 Bootstrap(app)
 
-class TextForm(FlaskForm):
-    text = StringField('Text', validators=[DataRequired()])
-    submit = SubmitField('Get Result')
-
-
-class Response(FlaskForm):
-    score = StringField('Score', validators=[DataRequired()])
-    sentiment = StringField('Sentiment', validators=[DataRequired()])
-    submit = SubmitField('Try Again')
-
-
-class Score(FlaskForm):
-    score = StringField('Score', validators=[DataRequired()])
-    submit = SubmitField('Try Again')
-
-
-class FileForm(FlaskForm):
+class UploadForm(FlaskForm):
     file_path = FileField('Choose dataset', validators=[
         DataRequired('Specify file'),
         FileAllowed(['csv'], 'CSV only!')
     ])
-    submit = SubmitField('Send File')
+    submit = SubmitField('Upload Data')
+
+class ParamsForm(FlaskForm):
+    model_list = ['Random Forest MSE', 'Gradient Boosting MSE']
+    model = SelectField('Select model', choices=model_list)
+    p1 = StringField('n_estimators', validators=[DataRequired()])
+    p2 = StringField('feature_subsample_size', validators=[DataRequired()])
+    p3 = StringField('max_depth', validators=[DataRequired()])
+    p4 = StringField('learning_rate (will be ignored for Random Forest)', validators=[DataRequired()])
+    submit = SubmitField('Train Model')
+
+
+class ValidateForm(FlaskForm):
+    file_path = FileField('Choose dataset', validators=[
+        DataRequired('Specify file'),
+        FileAllowed(['csv'], 'CSV only!')
+    ])
+    submit = SubmitField('Upload Data & Validate')
+    # score = StringField('Score', validators=[DataRequired()])
+    # sentiment = StringField('Sentiment', validators=[DataRequired()])
+    # submit = SubmitField('Try Again')
 
 class Dataset():
     df = None
+    X = None
+    y = None
+    val = None
 df = Dataset()
+
+class Model():
+    clf = None
+    params = {'n_estimators': 200, 'feature_subsample_size': 0.7, 'max_depth': 4, 'learning_rate': 0.05}
+clf = Model()
 
 @app.route('/', methods=['POST', 'GET'])
 def get_index():
-    file_form = FileForm()
-    score_form = Score()
+    file_form = UploadForm()
+    params_form = ParamsForm()
+    model_output = ''
+    val_form = ValidateForm()
 
     if request.method == 'POST' and file_form.validate_on_submit():
         stream = io.StringIO(file_form.file_path.data.stream.read().decode("UTF8"), newline=None)
         df.df = pd.read_csv(stream)
+        df.X = df.df.drop(['price'], axis='columns')
+        df.y = df.df['price']
+        df.X['date'] = pd.to_datetime(df.X['date']).dt.dayofyear
+        df.X = df.X.to_numpy()
+        df.y = df.y.to_numpy()
         print(f'Uploaded {df.df.shape} lines')
         return redirect(url_for('get_index'))
 
-    if request.method == 'POST' and score_form.validate_on_submit():
+    if request.method == 'POST' and params_form.validate_on_submit():
         try:
-            print('fiting...')
-            clf = RandomForestRegressor()
-            X, y = df.df.drop(['price', 'date'], axis='columns'), df.df['price']
-            clf.fit(X, y)
-            y_pred = clf.predict(X)
-            score_form.score.data = mean_squared_error(y_pred, y, squared=False)
-            print('validate...')
-            return render_template('index.html', file_form=file_form, score_form=score_form)
+            print('Set params...')
+            print(params_form.model.data)
+            n_estimators = int(params_form.p1.data)
+            feature_subsample_size = float(params_form.p2.data)
+            max_depth = int(params_form.p3.data)
+            learning_rate = float(params_form.p4.data)
+            if params_form.model == 'Random Forest MSE':
+                clf.clf = RandomForestMSE(n_estimators=n_estimators,
+                                            feature_subsample_size=feature_subsample_size,
+                                            max_depth=max_depth,
+                                            )
+            else:
+                clf.clf = GradientBoostingMSE(n_estimators=n_estimators,
+                                            feature_subsample_size=feature_subsample_size,
+                                            max_depth=max_depth,
+                                            learning_rate=learning_rate)
+            return redirect(url_for('get_train'))
+            # return render_template('train.html', model_output='')
         except Exception as exc:
             app.logger.info('Exception: {0}'.format(exc))
             print('Exception')
-    return render_template('index.html', file_form=file_form, score_form=score_form)
+    return render_template('index.html', file_form=file_form, params_form=params_form, val_form=val_form)
+
+@app.route('/train', methods=['POST', 'GET'])
+def get_train():
+    print('Ready for train')
+    clf.clf.fit(df.X, df.y)
+    print('Trained.')
+    y_pred = clf.clf.predict(df.X)
+    print('Validated.')
+    model_output = mean_squared_error(y_pred, df.y, squared=False)
+    return render_template('train.html', model_output=model_output)
